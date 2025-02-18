19b31e66df5c94add17ca0d702afc78fb01abb7c
[CLOUD4] 고범석 <kbssky94@gmail.com>
2025-02-18 17:34:45 +0900

    feat: yml, xml 로깅 단계 및 시스템 설정

--- src/main/java/com/hyetaekon/hyetaekon/HyetaekonApplication.java
+++ src/main/java/com/hyetaekon/hyetaekon/HyetaekonApplication.java
@@ -2,8 +2,14 @@ package com.hyetaekon.hyetaekon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableJpaAuditing
 @SpringBootApplication
+@EnableScheduling
+@EnableMongoRepositories
 public class HyetaekonApplication {
 
 	public static void main(String[] args) {
