e5b9e21ae9c844baa0ffcdad659c8c6f6e6ca277
[CLOUD4] 고범석 <kbssky94@gmail.com>
2025-02-24 16:22:06 +0900

    feat: 공공서비스 지원조건 + 서비스분야 Enum 작성

--- src/main/java/com/hyetaekon/hyetaekon/publicservice/entity/PublicService.java
+++ src/main/java/com/hyetaekon/hyetaekon/publicservice/entity/PublicService.java
@@ -1,12 +1,42 @@
 package com.hyetaekon.hyetaekon.publicservice.entity;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.hyetaekon.hyetaekon.publicservice.converter.BusinessTypeConverter;
+import com.hyetaekon.hyetaekon.publicservice.converter.FamilyTypeConverter;
+import com.hyetaekon.hyetaekon.publicservice.converter.OccupationConverter;
+import com.hyetaekon.hyetaekon.publicservice.converter.SpecialGroupConverter;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PublicService {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ServiceCategory serviceCategory;
+
+
+    @Convert(converter = SpecialGroupConverter.class)  // 명시적 선언
+    private SpecialGroup specialGroup;
+
+    @Convert(converter = FamilyTypeConverter.class)
+    private FamilyType familyType;
+
+    @Convert(converter = OccupationConverter.class)
+    private Occupation occupation;
+
+    @Convert(converter = BusinessTypeConverter.class)
+    private BusinessType businessType;
 
 }
