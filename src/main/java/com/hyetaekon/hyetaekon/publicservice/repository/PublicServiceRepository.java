5dbfc1c8b3e14948b9395b2e862cea6340cac955
[CLOUD4] 고범석 <kbssky94@gmail.com>
2025-02-26 00:40:39 +0900

    feat: 공공서비스 repository 생성

--- src/main/java/com/hyetaekon/hyetaekon/publicservice/repository/PublicServiceRepository.java
+++ src/main/java/com/hyetaekon/hyetaekon/publicservice/repository/PublicServiceRepository.java
@@ -1,4 +1,18 @@
 package com.hyetaekon.hyetaekon.publicservice.repository;
 
-public class PublicServiceRepository {
+
+import com.hyetaekon.hyetaekon.publicservice.entity.PublicService;
+import com.hyetaekon.hyetaekon.publicservice.entity.ServiceCategory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PublicServiceRepository extends JpaRepository<PublicService, Long> {
+    Page<PublicService> findByServiceCategory(ServiceCategory category, Pageable pageable);
+
+    List<PublicService> findTop6ByOrderByViewsDesc();
 }
