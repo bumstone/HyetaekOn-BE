d4717924cb38232f8059f7660976d47ef9294c8b
kobumseouk <kbssky2993@naver.com>
2025-03-27 14:49:15 +0900

    refactor: yml 수정, api엔드포인트 securityPath에 추가

--- src/main/java/com/hyetaekon/hyetaekon/common/config/SecurityPath.java
+++ src/main/java/com/hyetaekon/hyetaekon/common/config/SecurityPath.java
@@ -10,6 +10,7 @@ public class SecurityPath {
       "/api/token/refresh",
       "/api/users/check-duplicate",
       "/",
+      "/api/services",
       "/api/services/category/*",
       "/api/services/detail/*"
   };
@@ -17,16 +18,18 @@ public class SecurityPath {
 
   // hasRole("USER")
   public static final String[] USER_ENDPOINTS = {
-      "/api/users/me/bookmarked",
-      "/api/users/me/*",
+      "/api/users/me/**",
       "/api/users/me",
       "/api/logout",
-      "/api/services/popular"
+      "/api/services/popular",
+      "/api/services/*/bookmark",
+      "/api/interests",
+      "/api/interests/me"
   };
 
   // hasRole("ADMIN")
   public static final String[] ADMIN_ENDPOINTS = {
-      "/api/admin/**",
+      "/api/admin/users/**",
       "/api/public-data/serviceDetailList",
       "/api/public-data/supportConditionsList",
       "/api/public-data/serviceList/**"
