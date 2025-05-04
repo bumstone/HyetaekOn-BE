23644b15d3a25ac9346332c267de6a20f81271a1
[CLOUD4] 고범석 <kbssky94@gmail.com>
2025-05-04 18:34:50 +0900

    style: 사용하지 않는 부분 제거

--- src/main/java/com/hyetaekon/hyetaekon/user/repository/UserReportRepository.java
+++ src/main/java/com/hyetaekon/hyetaekon/user/repository/UserReportRepository.java
@@ -12,13 +12,4 @@ import org.springframework.stereotype.Repository;
 public interface UserReportRepository extends JpaRepository<UserReport, Long> {
     // 상태별 신고 목록 조회
     Page<UserReport> findByStatus(ReportStatus status, Pageable pageable);
-
-    // 특정 사용자가 신고한 내역 조회
-    Page<UserReport> findByReporter_Id(Long reporterId, Pageable pageable);
-
-    // 특정 사용자가 신고당한 내역 조회
-    Page<UserReport> findByReported_Id(Long reportedId, Pageable pageable);
-
-    // 신고 처리되지 않은 건수 확인
-    long countByStatus(ReportStatus status);
 }
