d7a05343fc5c3dc53554b642cec9520de70d01bd
kobumseouk <kbssky2993@gmail.com>
2025-05-12 04:50:39 +0900

    refactor: 공공데이터 동기화 스케줄러 매일 -> 매주로 변경

--- src/main/java/com/hyetaekon/hyetaekon/common/publicdata/schedule/PublicServiceDataScheduler.java
+++ src/main/java/com/hyetaekon/hyetaekon/common/publicdata/schedule/PublicServiceDataScheduler.java
@@ -15,13 +15,13 @@ public class PublicServiceDataScheduler {
     private final PublicServiceDataService publicServiceDataService;
 
     /**
-     * 매일 새벽 2시에 공공서비스 데이터 전체 동기화 실행
+     * 매주 월요일 새벽 2시에 공공서비스 데이터 전체 동기화 실행
      *  1. 서비스 목록 동기화
      *  2. 서비스 상세정보 동기화
      *  3. 서비스 지원조건 동기화
      *  4. 더 이상 사용되지 않는 데이터 정리
      */
-    @Scheduled(cron = "0 0 2 * * ?")
+    @Scheduled(cron = "0 0 2 ? * MON")
     public void syncAllPublicServiceData() {
         log.info("공공서비스 데이터 전체 동기화 스케줄러 시작");
 
